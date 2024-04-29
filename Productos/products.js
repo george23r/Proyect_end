5f7431fe6a496572494f5ccd7dbdc2c611ea469e
george23r <112.jor1020@gmail.com>
2024-04-29 14:34:04 -0500

    Create products.js
    El proyecto consiste en una tienda en línea para productos de drift. Se proporciona una lista de 50 productos con sus detalles, incluyendo nombre, categoría, precio en pesos colombianos, código y una imagen predeterminada. La tienda tiene como objetivo ofrecer una amplia gama de productos para entusiastas del drift, desde accesorios hasta repuestos y mantenimiento.

---
+++ Productos/products.js
@@ -0,0 +1,354 @@
+const driftProducts = [
+    {
+      name: "Llantas de alto rendimiento",
+      category: "Accesorios",
+      image: "Seleccionar imagen",
+      price: "250000",
+      code: "PRD001AB"
+    },
+    {
+      name: "Suspensión ajustable",
+      category: "Accesorios",
+      image: "Seleccionar imagen",
+      price: "500000",
+      code: "PRD002CD"
+    },
+    {
+      name: "Escape deportivo",
+      category: "Accesorios",
+      image: "Seleccionar imagen",
+      price: "700000",
+      code: "PRD003EF"
+    },
+    {
+      name: "Difusor trasero",
+      category: "Accesorios",
+      image: "Seleccionar imagen",
+      price: "400000",
+      code: "PRD004GH"
+    },
+    {
+      name: "Asientos de cubo",
+      category: "Accesorios",
+      image: "Seleccionar imagen",
+      price: "300000",
+      code: "PRD005IJ"
+    },
+    {
+      name: "Volante deportivo desplazado",
+      category: "Accesorios",
+      image: "Seleccionar imagen",
+      price: "150000",
+      code: "PRD006KL"
+    },
+    {
+      name: "Kit de carrocería",
+      category: "Accesorios",
+      image: "Seleccionar imagen",
+      price: "1200000",
+      code: "PRD007MN"
+    },
+    {
+      name: "Juego de luces LED",
+      category: "Accesorios",
+      image: "Seleccionar imagen",
+      price: "100000",
+      code: "PRD008OP"
+    },
+    {
+      name: "Controlador de freno de mano hidráulico",
+      category: "Accesorios",
+      image: "Seleccionar imagen",
+      price: "600000",
+      code: "PRD009QR"
+    },
+    {
+      name: "Kit de refuerzo de chasis",
+      category: "Accesorios",
+      image: "Seleccionar imagen",
+      price: "800000",
+      code: "PRD010ST"
+    },
+    {
+      name: "Sistema de refrigeración mejorado",
+      category: "Accesorios",
+      image: "Seleccionar imagen",
+      price: "900000",
+      code: "PRD011UV"
+    },
+    {
+      name: "Pastillas de freno de alto rendimiento",
+      category: "Accesorios",
+      image: "Seleccionar imagen",
+      price: "150000",
+      code: "PRD012WX"
+    },
+    {
+      name: "Kit de control de tracción ajustable",
+      category: "Accesorios",
+      image: "Seleccionar imagen",
+      price: "400000",
+      code: "PRD013YZ"
+    },
+    {
+      name: "Kit de aumento de potencia",
+      category: "Accesorios",
+      image: "Seleccionar imagen",
+      price: "2000000",
+      code: "PRD014ABCD"
+    },
+    {
+      name: "Sistema de extinción de incendios",
+      category: "Accesorios",
+      image: "Seleccionar imagen",
+      price: "1200000",
+      code: "PRD015EFGH"
+    },
+    {
+      name: "Frenos de disco perforados",
+      category: "Accesorios",
+      image: "Seleccionar imagen",
+      price: "300000",
+      code: "PRD016IJKL"
+    },
+    {
+      name: "Barra antivuelco",
+      category: "Accesorios",
+      image: "Seleccionar imagen",
+      price: "700000",
+      code: "PRD017MNOP"
+    },
+    {
+      name: "Sistema de escape de alto flujo",
+      category: "Accesorios",
+      image: "Seleccionar imagen",
+      price: "600000",
+      code: "PRD018QRST"
+    },
+    {
+      name: "Kit de freno de mano hidráulico",
+      category: "Accesorios",
+      image: "Seleccionar imagen",
+      price: "800000",
+      code: "PRD019UVWX"
+    },
+    {
+      name: "Neumáticos de competición",
+      category: "Accesorios",
+      image: "Seleccionar imagen",
+      price: "350000",
+      code: "PRD020YZAB"
+    },
+    {
+      name: "Luces traseras LED",
+      category: "Accesorios",
+      image: "Seleccionar imagen",
+      price: "200000",
+      code: "PRD021CDEF"
+    },
+    {
+      name: "Kit de admisión de aire frío",
+      category: "Accesorios",
+      image: "Seleccionar imagen",
+      price: "500000",
+      code: "PRD022GHIJ"
+    },
+    {
+      name: "Amortiguadores ajustables",
+      category: "Accesorios",
+      image: "Seleccionar imagen",
+      price: "450000",
+      code: "PRD023KLMN"
+    },
+    {
+      name: "Kit de luces estroboscópicas",
+      category: "Accesorios",
+      image: "Seleccionar imagen",
+      price: "250000",
+      code: "PRD024OPQR"
+    },
+    {
+      name: "Barras estabilizadoras",
+      category: "Accesorios",
+      image: "Seleccionar imagen",
+      price: "300000",
+      code: "PRD025STUV"
+    },
+    {
+      name: "Sistema de audio de alto rendimiento",
+      category: "Accesorios",
+      image: "Seleccionar imagen",
+      price: "700000",
+      code: "PRD026WXYZ"
+    },
+    {
+      name: "Kit de nitroso",
+      category: "Accesorios",
+      image: "Seleccionar imagen",
+      price: "1500000",
+      code: "PRD027ABCDE"
+    },
+    {
+      name: "Barras de torre",
+      category: "Accesorios",
+      image: "Seleccionar imagen",
+      price: "250000",
+      code: "PRD028FGHIJ"
+    },
+    {
+      name: "Aceite de motor sintético",
+      category: "Mantenimiento",
+      image: "Seleccionar imagen",
+      price: "100000",
+      code: "PRD029KLMN"
+    },
+    {
+      name: "Filtro de aire de alto flujo",
+      category: "Mantenimiento",
+      image: "Seleccionar imagen",
+      price: "150000",
+      code: "PRD030OPQR"
+    },
+    {
+      name: "Líquido de frenos de competición",
+      category: "Mantenimiento",
+      image: "Seleccionar imagen",
+      price: "80000",
+      code: "PRD031STUV"
+    },
+    {
+      name: "Bujías de alto rendimiento",
+      category: "Mantenimiento",
+      image: "Seleccionar imagen",
+      price: "50000",
+      code: "PRD032WXYZ"
+    },
+    {
+      name: "Aceite de caja de cambios sintético",
+      category: "Mantenimiento",
+      image: "Seleccionar imagen",
+      price: "120000",
+      code: "PRD033ABCDE"
+    },
+    {
+      name: "Filtro de aceite de competición",
+      category: "Mantenimiento",
+      image: "Seleccionar imagen",
+      price: "70000",
+      code: "PRD034FGHIJ"
+    },
+    {
+      name: "Líquido de dirección asistida de alto rendimiento",
+      category: "Mantenimiento",
+      image: "Seleccionar imagen",
+      price: "90000",
+      code: "PRD035KLMNO"
+    },
+    {
+      name: "Líquido refrigerante de competición",
+      category: "Mantenimiento",
+      image: "Seleccionar imagen",
+      price: "80000",
+      code: "PRD036PQRST"
+    },
+    {
+      name: "Aceite para diferencial de competición",
+      category: "Mantenimiento",
+      image: "Seleccionar imagen",
+      price: "150000",
+      code: "PRD037UVWXY"
+    },
+    {
+      name: "Filtro de combustible de alto flujo",
+      category: "Mantenimiento",
+      image: "Seleccionar imagen",
+      price: "60000",
+      code: "PRD038ZABC"
+    },
+    {
+      name: "Líquido de transmisión de competición",
+      category: "Mantenimiento",
+      image: "Seleccionar imagen",
+      price: "130000",
+      code: "PRD039DEFG"
+    },
+    {
+      name: "Aceite de dirección asistida de alto rendimiento",
+      category: "Mantenimiento",
+      image: "Seleccionar imagen",
+      price: "100000",
+      code: "PRD040HIJK"
+    },
+    {
+      name: "Limpiador de inyectores",
+      category: "Mantenimiento",
+      image: "Seleccionar imagen",
+      price: "70000",
+      code: "PRD041LMNO"
+    },
+    {
+      name: "Aditivo para gasolina de competición",
+      category: "Mantenimiento",
+      image: "Seleccionar imagen",
+      price: "50000",
+      code: "PRD042PQRS"
+    },
+    {
+      name: "Aceite para sistema de dirección asistida",
+      category: "Mantenimiento",
+      image: "Seleccionar imagen",
+      price: "80000",
+      code: "PRD043TUVW"
+    },
+    {
+      name: "Grasa para rodamientos de alto rendimiento",
+      category: "Mantenimiento",
+      image: "Seleccionar imagen",
+      price: "60000",
+      code: "PRD044XYZA"
+    },
+    {
+      name: "Limpiador de filtro de aire",
+      category: "Mantenimiento",
+      image: "Seleccionar imagen",
+      price: "40000",
+      code: "PRD045BCDE"
+    },
+    {
+      name: "Aceite para sistema de transmisión automática",
+      category: "Mantenimiento",
+      image: "Seleccionar imagen",
+      price: "110000",
+      code: "PRD046FGHI"
+    },
+    {
+      name: "Limpiador de sistema de combustible",
+      category: "Mantenimiento",
+      image: "Seleccionar imagen",
+      price: "50000",
+      code: "PRD047JKLM"
+    },
+    {
+      name: "Lubricante de cadena de competición",
+      category: "Mantenimiento",
+      image: "Seleccionar imagen",
+      price: "70000",
+      code: "PRD048NOPQ"
+    },
+    {
+      name: "Aceite para caja de cambios manual de alto rendimiento",
+      category: "Mantenimiento",
+      image: "Seleccionar imagen",
+      price: "120000",
+      code: "PRD049RSTU"
+    },
+    {
+      name: "Filtro de aire de competición",
+      category: "Mantenimiento",
+      image: "Seleccionar imagen",
+      price: "80000",
+      code: "PRD050VWXY"
+    },
+    // Aquí continúa la lista de productos...
+  ];
+  
